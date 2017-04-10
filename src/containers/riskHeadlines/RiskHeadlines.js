956e90a60caec87731d099f3e7be2acba84a1525
万俐君 <lijun.wan@socialcredits.cn>
2017-04-10 09:50:31 +0800

    规范代码

--- src/containers/riskHeadlines/RiskHeadlines.js
+++ src/containers/riskHeadlines/RiskHeadlines.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import RiskMain from 'components/riskHeadlines';
+import RiskMain from 'components/riskHeadlines/RiskMain';
 @observer
 export default class RiskHeadlines extends Component {
   render() {
