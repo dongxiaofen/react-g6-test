a29136a78aac6bbd5c9a32594bdc078cd7944b9d
changhua.li <changhua.li@socialcredits.cn>
2017-04-13 15:25:44 +0800

    监控列表api 添加cancel

--- src/api/monitorList.js
+++ src/api/monitorList.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 export const getMonitorCount = (params) => {
-  return axios.get('/api/monitor/statistic', {params: params});
+  return axios.get('/api/monitor/statistic', {
+    params: params,
+  });
 };
-export const getMainList = (params) => {
-  return axios.get('/api/monitor/companyList/xx', {params: params});
+export const getMainList = (params, source) => {
+  return axios.get('/api/monitor/companyList/xx', {
+    params: params,
+    cancelToken: source.token
+  });
 };
 export const getRelList = (monitorId) => {
   return axios.get(`/api/monitor/${monitorId}/companyList`);
--- src/stores/monitorList.js
+++ src/stores/monitorList.js
@@ -1,9 +1,10 @@
 import { observable, action } from 'mobx';
-// import axios from 'axios';
+import axios from 'axios';
 import pathval from 'pathval';
 import { monitorListApi } from 'api';
-
+const CancelToken = axios.CancelToken;
 class MonitorListStore {
+  axiosCancel = [];
   @observable searchInput = '';
   @observable sortDirection = {
     start_tm: 'DESC',
@@ -31,31 +32,41 @@ class MonitorListStore {
   @action.bound getMainCount() {
     const {monitorStatus, companyName} = this.searchParams;
     monitorListApi.getMonitorCount({monitorStatus, companyName})
-    .then(action('getCount_success', resp => {
-      this.monitorCount = resp.data;
-    }))
-    .catch(action('getCount_error', err => {
-      this.monitorCount = err.response.data;
-    }));
+      .then(action('getCount_success', resp => {
+        this.monitorCount = resp.data;
+      }))
+      .catch(action('getCount_error', err => {
+        this.monitorCount = err.response.data;
+      }));
   }
   @action.bound getMainList() {
+    if (this.axiosCancel[0]) {
+      const cancel = this.axiosCancel.pop();
+      cancel();
+    }
+    const source = CancelToken.source();
     const mainParams = this.searchParams;
-    monitorListApi.getMainList(mainParams)
-    .then(action('getMainList_success', resp => {
-      this.mainList = resp.data;
-    }))
-    .catch(action('getMainList_error', err => {
-      this.mainList = err.response.data;
-    }));
+    this.axiosCancel.push(source.cancel);
+    monitorListApi.getMainList(mainParams, source)
+      .then(action('getMainList_success', resp => {
+        this.axiosCancel.pop();
+        this.mainList = resp.data;
+      }))
+      .catch(action('getMainList_error', err => {
+        if (!axios.isCancel(err)) {
+          this.axiosCancel.pop();
+          this.mainList = err.response.data;
+        }
+      }));
   }
   @action.bound getRelationList(monitorId) {
     monitorListApi.getRelList(monitorId)
-    .then(action('getRelList_success', resp => {
-      this.relationList.set(monitorId, resp.data);
-    }))
-    .catch(action('getRelList_success', err => {
-      this.relationList.set(monitorId, err.response.data);
-    }));
+      .then(action('getRelList_success', resp => {
+        this.relationList.set(monitorId, resp.data);
+      }))
+      .catch(action('getRelList_success', err => {
+        this.relationList.set(monitorId, err.response.data);
+      }));
   }
   @action.bound delRelationList(monitorId) {
     this.relationList[monitorId] = null;
