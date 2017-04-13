0b35bb65db70da7373257a233ffbe057decef44f
dengfuqiu <kmdengfuqiu@126.com>
2017-04-13 13:41:17 +0800

    合并冲突

--- src/stores/index.js
+++ src/stores/index.js
@@ -9,10 +9,7 @@ export myHomePageStore from './myHomePage';
 export clientStore from './client';
 export riskHeadlinesStore from './riskHeadlines';
 export leftBarStore from './leftBar';
-<<<<<<< HEAD
 export detailModalStore from './detailModal';
-=======
 export corpDetailStore from './report/corpDetail';
 export riskStore from './report/risk';
->>>>>>> xx-mobx-2.0.0-dev
 // append here from plop
