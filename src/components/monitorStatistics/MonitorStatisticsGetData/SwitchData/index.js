a5a228379b873c0f01ce8607f7572449700d9121
dengfuqiu <kmdengfuqiu@126.com>
2017-04-14 13:57:27 +0800

    添加select组件，monitorStatistics组件

---
+++ src/components/monitorStatistics/MonitorStatisticsGetData/SwitchData/index.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+import { observer } from 'mobx-react';
+import styles from './index.less';
+
+function SwitchData({}) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+SwitchData.propTypes = {
+  foo: PropTypes.string,
+};
+export default observer(SwitchData);
