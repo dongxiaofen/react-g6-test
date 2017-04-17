d0e64ff4f006cf537e0c70b0d4b7a055ca58d101
谢明 <ming.xie@socialcredits.cn>
2017-04-17 11:25:30 +0800

    修改图片路径

--- src/components/myHomePage/AlertInfo/BasicList/index.js
+++ src/components/myHomePage/AlertInfo/BasicList/index.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import { observer, inject } from 'mobx-react';
 import styles from './index.less';
+import pathval from 'pathval';
 
 function BasicList({ routing, alertData }) {
   const degreeHandle = (type, degree) => {
@@ -25,21 +26,20 @@ function BasicList({ routing, alertData }) {
   const borderStyle = degreeHandle('border', 'LOW');
   return (
     <li className={styles.AlertEach}>
-      <div className={styles.AlertEachDiv + ' ' + borderStyle}>
-        <div className={styles.AlertEachKind}>
-         <span>
-          <span>{'无'}</span>
-          <label>（'')}）</label>
-         </span>
+      <div className={styles.AlertEachDiv + ' ' + styles.AlertEachBorderHIGH}>
+        <div className={styles.AlertEachKind}><span>
+                      <span>{alertData.ruleName ? alertData.ruleName : '无'}</span>
+                      <label>（风险关联企业：{pathval.getPathValue(alertData, 'content.blackListComp') || '无'};关联层级：{pathval.getPathValue(alertData, 'content.level') || '无'}）</label>
+                    </span>
         </div>
         <div className={styles.AlertEachInfo}>
-                  <span style={{width: '50%'}}>
-                    企业：<label><a onClick={turnToMonitor.bind(this, 14)}>{'无'}</a></label>
-                  </span><span style={{width: '25%'}}>
-                    类型：<label>{'无'}</label>
-                  </span><span style={{textAlign: 'right', width: '25%'}}>
-                    时间：{'无'}
-                  </span>
+                    <span style={{width: '50%'}}>
+                      企业：<label><a onClick={turnToMonitor.bind(this, alertData.monitorId, 'risk', pathval.getPathValue(alertData, 'content.blackListComp'))}>{alertData.companyName || '无'}</a></label>
+                    </span><span style={{width: '25%'}}>
+                      类型：<label>风险关联预警</label>
+                    </span><span style={{textAlign: 'right', width: '25%'}}>
+                      时间：{alertData.alertTime || '无'}
+                    </span>
         </div>
       </div>
     </li>
