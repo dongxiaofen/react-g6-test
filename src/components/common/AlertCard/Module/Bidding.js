3b4c1040d1f66ba86a806127c9fdb05f20d5c418
万俐君 <lijun.wan@socialcredits.cn>
2017-04-17 13:37:23 +0800

    重构bidding

--- src/components/common/AlertCard/Module/Bidding.js
+++ src/components/common/AlertCard/Module/Bidding.js
@@ -1,67 +1,61 @@
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
 import BaseModule from '../BaseModule';
-export default class Bidding extends Component {
-  static propTypes = {
-    data: PropTypes.object,
-    commonBoundAC: PropTypes.object,
-    module: PropTypes.string,
-    reducerData: PropTypes.object,
+import {observer} from 'mobx-react';
+function Bidding({data, module, reducerData, store}) {
+  const viewDetail = () => {
+    // let companyId = '';
+    // const actionId = Date.now();
+    // if (module === 'laneGraph') {
+    //   if (data.relatedMonitorId) {
+    //     companyId = data.relatedMonitorId;
+    //   } else {
+    //     companyId = data.mainMonitorId;
+    //   }
+    // } else {
+    //   companyId = reducerData.info.monitorId;
+    // }
+    // const announceId = data.content.announceID;
+    // const getUrl = `/api/monitor/${companyId}/operation/bidding/detail?announceId=${announceId}`;
+    console.log(store);
+    // commonBoundAC.getDetail(
+    //   getUrl,
+    //   ['bidMarkertDetailData', 'result'],
+    //   'DETAILS_MODAL_UPDATE',
+    //   data,
+    //   actionId,
+    //   data.content.url,
+    //   './bidMarket/bidMarketTitle',
+    //   './bidMarket/bidMarketContent',
+    //   './bidMarket/bidMarketSource',
+    //   'RISK_UPDATE_VALUE',
+    //   ['events', 'loading', data.eventId]
+    // );
   };
-  viewDetail = () => {
-    let companyId = '';
-    const actionId = Date.now();
-    if (this.props.module === 'laneGraph') {
-      if (this.props.data.get('relatedMonitorId')) {
-        companyId = this.props.data.get('relatedMonitorId');
-      } else {
-        companyId = this.props.data.get('mainMonitorId');
-      }
-    } else {
-      companyId = this.props.reducerData.info.monitorId;
-    }
-    const announceId = this.props.data.content.announceID;
-    const getUrl = `/api/monitor/${companyId}/operation/bidding/detail?announceId=${announceId}`;
-    this.props.commonBoundAC.getDetail(
-      getUrl,
-      ['bidMarkertDetailData', 'result'],
-      'DETAILS_MODAL_UPDATE',
-      this.props.data,
-      actionId,
-      this.props.data.content.url,
-      './bidMarket/bidMarketTitle',
-      './bidMarket/bidMarketContent',
-      './bidMarket/bidMarketSource',
-      'RISK_UPDATE_VALUE',
-      ['events', 'loading', this.props.data.eventId]
-    );
-  }
-  render() {
-    const eventId = this.props.data.eventId;
-    const data = {
-      'hideConfig': [
-        {'key': 'title', 'width': '12'},
-      ],
-      'viewConfig': [
-        {'key': 'title', 'width': '12'},
-        {'key': 'publishDate', 'width': '12'},
-        {'key': 'participator', 'width': '12'},
-      ],
-      date: {
-        label: '成交日期',
-        value: this.props.data.content.date
-      },
-      'handleBlock': true,
-      'actionToUrl': true,
-      'dict': 'bidding',
-      'items': this.props.data,
-    };
-    return (<BaseModule
-              type="bidding"
-              {...this.props}
-              data={data}
-              viewDetCallback={this.viewDetail}
-              type="double"
-              module={this.props.module}
-              loading={this.props.reducerData.loading[eventId]}/>);
-  }
+  const eventId = data.eventId;
+  const moduleData = {
+    'hideConfig': [
+      {'key': 'title', 'width': '12'},
+    ],
+    'viewConfig': [
+      {'key': 'title', 'width': '12'},
+      {'key': 'publishDate', 'width': '12'},
+      {'key': 'participator', 'width': '12'},
+    ],
+    date: {
+      label: '成交日期',
+      value: data.content.date
+    },
+    'handleBlock': true,
+    'actionToUrl': true,
+    'dict': 'bidding',
+    'items': data,
+  };
+  return (<BaseModule
+            type="bidding"
+            data={moduleData}
+            viewDetCallback={viewDetail}
+            type="double"
+            module={module}
+            loading={reducerData.loading[eventId]}/>);
 }
+export default observer(Bidding);
