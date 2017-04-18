1c0aa99195ee2b9a4c6113365f4b781dd1437c45
谢明 <ming.xie@socialcredits.cn>
2017-04-18 16:01:06 +0800

    在报告列表加入分页

--- src/components/reportManage/TableList/index.js
+++ src/components/reportManage/TableList/index.js
@@ -2,14 +2,33 @@ import React, {PropTypes} from 'react';
 import { observer, inject } from 'mobx-react';
 import BaseList from '../BaseList';
 import pathval from 'pathval';
+import Pagination from 'components/lib/pagination';
+import { runInAction } from 'mobx';
+import styles from './index.less';
 
 function TableList({ reportManageStore }) {
+  const changePages = (newPage) => {
+    runInAction('newPage...', () => {
+      pathval.setPathValue(reportManageStore, 'params.index', newPage);
+    });
+    reportManageStore.getReportList(pathval.getPathValue(this.props, 'reportManageStore.params'));
+  };
   return (
   <div>
-  {
-    pathval.getPathValue(reportManageStore, 'list.data.content') ? pathval.getPathValue(reportManageStore, 'list.data.content').map((item, index) =>
-      <BaseList key={`reportManage${index}Id`} listData={item}/>) : ''
-  }
+    <div className={styles.table}>
+      {
+        pathval.getPathValue(reportManageStore, 'list.data.content') ? pathval.getPathValue(reportManageStore, 'list.data.content').map((item, index) =>
+          <BaseList key={`reportManage${index}Id`} listData={item}/>) : ''
+      }
+    </div>
+    <div className={styles.pages}>
+      <Pagination
+        current={pathval.getPathValue(reportManageStore, 'params.index')}
+        pageSize={10}
+        total={pathval.getPathValue(reportManageStore, 'list.data.totalElements') ? pathval.getPathValue(reportManageStore, 'list.data.totalElements') : 0}
+        onChange={changePages.bind(this)}
+      />
+    </div>
   </div>
   );
 }
