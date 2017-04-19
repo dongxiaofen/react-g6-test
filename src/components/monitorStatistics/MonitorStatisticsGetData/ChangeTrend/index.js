b514b6cdbfa1bf7b98772a4120e08cb8bf28bdbb
dengfuqiu <kmdengfuqiu@126.com>
2017-04-19 11:45:02 +0800

    把chart相关的js给引入进去了

--- src/components/monitorStatistics/MonitorStatisticsGetData/ChangeTrend/index.js
+++ src/components/monitorStatistics/MonitorStatisticsGetData/ChangeTrend/index.js
@@ -4,14 +4,25 @@ import { observer } from 'mobx-react';
 import styles from './index.less';
 import Row from 'components/common/layout/Row';
 import Col from 'components/common/layout/Col';
+import BaseChart from 'components/common/Charts/BaseChart';
 
 function ChangeTrend({ changeTrend }) {
+  console.log(changeTrend.result.length);
   return (
-    <Row>
-      <Col>
-      
-      </Col>
-    </Row>
+    <div className={styles.statisticItem}>
+      <Row>
+        <Col width="9">
+          this is changeTrend
+          {/*<BaseChart
+            chartId="changeTrend"
+            height="400px"
+            option={ changeTrend.chartOption }/>*/}
+        </Col>
+        <Col width="3">
+          this is changeTrend table
+        </Col>
+      </Row>
+    </div>
   );
 }
 
