c790fa426e5f3cfc7afcf56d9ec38707a90c4568
swufewyd <swufewyd@gmail.com>
2017-04-19 12:01:33 +0800

    删掉重复的isFirstLoad

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -20,9 +20,6 @@ export default class Html extends Component {
     isDev: PropTypes.bool,
     reqPathName: PropTypes.string,
   };
-  isFirstLoad() {
-    return this.props.pdfDown === '1' || this.props.reqPathName === '/';
-  }
   prepareStore(allStore) {
     const keyArr = Object.keys(allStore);
     const output = {};
