0f9a078def8c96c4b9a2ebb5605747a89ee48936
dengfuqiu <kmdengfuqiu@126.com>
2017-04-21 10:54:56 +0800

    添加navbar相关

---
+++ src/components/NavBar/Menu/index.js
@@ -0,0 +1,134 @@
+import React, {PropTypes} from 'react';
+import { observer } from 'mobx-react';
+import styles from './index.less';
+
+function Menu({ routing }) {
+  const config = [
+    { parent: { module: '首页', route: 'myHomePage' } },
+    { parent: { module: '查询', route: 'search' } },
+    { parent: { module: '报告', route: 'reportManage' } },
+    {
+      parent: { module: '监控' },
+      children: [
+        { module: '每日监控', route: 'riskHeadlines' },
+        { module: '监控统计', route: 'headTrend' },
+        { module: '监控列表', route: 'monitorList' },
+      ]
+    },
+    {
+      parent: { module: '市场' },
+      children: [
+        { module: '招投标', route: 'bidMarket' },
+        { module: '资产交易', route: 'poorAssets' },
+        { module: '风险企业', route: 'blackList' },
+      ],
+    },
+    {
+      parent: { module: '设置' },
+      children: [
+        { module: '预警设置', route: 'ruleList' },
+        { module: '账号设置', route: 'account' },
+      ],
+    },
+  ];
+
+  const routeToPage = (route) => {
+    this.props.history.push(`/${route}`);
+  };
+
+  const childrenIsActiveFun = (route) => {
+    const pathname = routing.location.pathname.substr(1);
+    return route === pathname;
+  };
+
+  const parentItemIsActiveFun = (item) => {
+    const pathname = routing.location.pathname.substr(1);
+    const children = item.children;
+    if (children && children.length > 0) {
+      const isHas = children.find((child) => {
+        return child.route === pathname;
+      });
+      return isHas !== undefined ? true : false;
+    }
+    return item.parent.route === pathname;
+  };
+
+  const parentItemMouseOver = (pIdx) => {
+    const parentItemRef = this.refs[`parentItem${pIdx}`];
+    const preClass = parentItemRef.className;
+    const newClass = `${preClass} ${styles.parentItemHover}`;
+    parentItemRef.className = newClass;
+  };
+
+  const parentItemMouseOut = (pIdx) => {
+    const parentItemRef = this.refs[`parentItem${pIdx}`];
+    const newClass = parentItemRef.className.split(' ')[0];
+    parentItemRef.className = newClass;
+  };
+
+  const createItem = () => {
+    const output = [];
+    config.forEach((item, pIdx) => {
+      let childrenOutput = '';
+      const childrenArray = [];
+      const parent = item.parent;
+      const children = item.children;
+      if (children && children.length > 0) {
+        children.forEach((childItem, childIdx) => {
+          const childrenIsActive = childrenIsActiveFun(childItem.route);
+          const itemCss = childrenIsActive ? styles.childItemActive : styles.childItem;
+          childrenArray.push(
+            <div key={`child${childIdx}`} className={itemCss} onClick={routeToPage.bind(this, childItem.route)}>
+              {childItem.module}
+            </div>
+          );
+        });
+        childrenOutput = <div key={`childBox${pIdx}`} className={styles.childBox}>{childrenArray}</div>;
+      }
+      const parentItemIsActive = parentItemIsActiveFun(item) ? styles.parentItemActive : styles.parentItem;
+      const parentItemDOM = parent.route
+        ?
+        <div className={parentItemIsActive} onClick={routeToPage.bind(this, parent.route)}>{parent.module}</div>
+        :
+        <div className={parentItemIsActive}>
+          {parent.module}
+          <span className={styles.parentIcon}></span>
+        </div>;
+      if (childrenOutput) {
+        output.push(
+          <div
+            key={`parent${pIdx}`}
+            ref={`parentItem${pIdx}`}
+            className={styles.navbarItem}
+            onMouseOver={parentItemMouseOver.bind(this, pIdx)}
+            onMouseOut={parentItemMouseOut.bind(this, pIdx)}>
+            {parentItemDOM}
+            {childrenOutput}
+          </div>
+        );
+      } else {
+        output.push(
+          <div key={`parent${pIdx}`} className={styles.navbarItem}>
+            {parentItemDOM}
+          </div>
+        );
+      }
+    });
+    return output;
+  };
+  return (
+    <div>
+      <div className={styles.logo}>
+        <div className={styles.logoImg}></div>
+      </div>
+      <div className={`clearfix ${styles['navbar-left']}`}>
+        {createItem()}
+      </div>
+    </div>
+  );
+}
+
+Menu.propTypes = {
+  routing: PropTypes.object,
+};
+export default observer(Menu);
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,44 @@
-import React, {PropTypes} from 'react';
-import { observer } from 'mobx-react';
-import styles from './index.less';
+// import React, {PropTypes} from 'react';
+// import { observer, inject } from 'mobx-react';
 
-function NavBar({}) {
-  return (
-    <div className={styles}>
-      this is NavBar
-    </div>
-  );
+// import styles from './index.less';
+// import Container from 'components/common/layout/Container';
+
+// import Menu from './Menu';
+
+// function NavBar({ routing }) {
+//   return (
+//     <div className={`clearfix ${styles.menu}`}>
+//       <Container>
+//         {/*<Menu routing={routing} />*/}
+//         <div className={`clearfix ${styles['menu-right']}`}>
+//         </div>
+//       </Container>
+//       <div className={styles.menuBottomBorder}></div>
+//     </div>
+//   );
+// }
+
+// NavBar.propTypes = {
+//   routing: PropTypes.object,
+// };
+// export default inject('routing')(observer(NavBar));
+
+import React, { Component, PropTypes } from 'react';
+import { observer, inject } from 'mobx-react';
+@inject('routing')
+@observer
+export default class NavBar extends Component {
+  static propTypes = {
+    routing: PropTypes.object,
+  }
+  render() {
+    console.log(this.props.routing, '--------------------------routing');
+    return (
+      <div>
+      xxxxx
+        this is NavBar
+      </div>
+    );
+  }
 }
-
-NavBar.propTypes = {
-  foo: PropTypes.string,
-};
-export default observer(NavBar);
