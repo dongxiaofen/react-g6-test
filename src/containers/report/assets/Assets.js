9039e1e67b702f24858c671b3c8f67225280e28c
谢明 <ming.xie@socialcredits.cn>
2017-04-25 18:31:19 +0800

    ‘修改tabs’

--- src/containers/report/assets/Assets.js
+++ src/containers/report/assets/Assets.js
@@ -1,12 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { observer, inject } from 'mobx-react';
-import Tabs from 'components/lib/tabs';
 import Trademark from 'components/companyHome/report/assets/bidding/Trademark';
 import Patent from 'components/companyHome/report/assets/bidding/Patent';
 import Bidding from 'components/companyHome/report/assets/bidding/Bidding';
-import { batchReport } from 'components/hoc';
-
+import Tabs from 'antd/lib/tabs';
 const TabPane = Tabs.TabPane;
+import { batchReport } from 'components/hoc';
 
 @inject('routing', 'assetsStore', 'uiStore')
 @batchReport('assets')
@@ -18,8 +17,8 @@ export default class Assets extends Component {
   render() {
     const assetsStore = this.props.assetsStore;
     return (
-      <Tabs>
-        <TabPane tab="无形资产/招投标">
+      <Tabs defaultActiveKey="无形资产/招投标">
+        <TabPane tab="无形资产/招投标" key="无形资产/招投标">
           <Trademark trademarkInfo={assetsStore.trademarkData}
                      isLoading={assetsStore.trLoading}
                      error={false}
