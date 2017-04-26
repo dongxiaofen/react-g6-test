4dee5bb11dcd719b00f13132d29a9cc6676a46c5
万俐君 <lijun.wan@socialcredits.cn>
2017-04-26 16:48:10 +0800

    2.2.0

---
+++ src/components/companyHome/report/risk/Court/CourtAnnouncement/index.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+import { observer } from 'mobx-react';
+
+function CourtAnnouncement({}) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+CourtAnnouncement.propTypes = {
+  foo: PropTypes.string,
+};
+export default observer(CourtAnnouncement);
