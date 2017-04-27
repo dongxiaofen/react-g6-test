fa73d669abc72221f643e458207b707be7fe61d0
changhua.li <changhua.li@socialcredits.cn>
2017-04-27 14:26:27 +0800

    新闻添加统计数 新闻store添加resetStore方法

--- src/components/companyHome/report/internet/news/Content/NewsTabs/index.js
+++ src/components/companyHome/report/internet/news/Content/NewsTabs/index.js
@@ -22,14 +22,24 @@ function NewsTabs({internetStore, uiStore, routing}) {
       internetStore.getInternet(params);
     }
   };
+  const getCount = key => {
+    let count;
+    for (let idx = 0; idx < statistic.length; idx++) {
+      if (statistic[idx].key === key) {
+        count = statistic[idx].count;
+        break;
+      }
+    }
+    return count || 0;
+  };
   const tabData = [
-    {label: '全部新闻', key: 'ALL'},
-    {label: '企业战略', key: 'NEWS_STRATEGY'},
-    {label: '诉讼纠纷', key: 'NEWS_LITIGATION'},
-    {label: '经营状况', key: 'NEWS_BUSINESS'},
-    {label: '品牌产品', key: 'NEWS_BRAND'},
-    {label: '人事变动', key: 'NEWS_PERSONNEL'},
-    {label: '其他新闻', key: 'NEWS_OTHER'},
+    {label: '全部新闻', key: 'ALL', number: getCount('ALL')},
+    {label: '企业战略', key: 'NEWS_STRATEGY', number: getCount('NEWS_STRATEGY')},
+    {label: '诉讼纠纷', key: 'NEWS_LITIGATION', number: getCount('NEWS_LITIGATION')},
+    {label: '经营状况', key: 'NEWS_BUSINESS', number: getCount('NEWS_BUSINESS')},
+    {label: '品牌产品', key: 'NEWS_BRAND', number: getCount('NEWS_BRAND')},
+    {label: '人事变动', key: 'NEWS_PERSONNEL', number: getCount('NEWS_PERSONNEL')},
+    {label: '其他新闻', key: 'NEWS_OTHER', number: getCount('NEWS_OTHER')},
   ];
   return (
     <SimpleTabs data={tabData} active={activeType} clickHandel={typeHandle} />
