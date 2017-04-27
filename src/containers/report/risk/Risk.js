4485c877d6d0359c89f836597a9db5176badadb0
万俐君 <lijun.wan@socialcredits.cn>
2017-04-27 14:34:34 +0800

    更换TAB

--- src/containers/report/risk/Risk.js
+++ src/containers/report/risk/Risk.js
@@ -4,7 +4,7 @@ import Court from 'components/companyHome/report/risk/Court';
 import Tax from 'components/companyHome/report/risk/Tax';
 import Mici from 'components/companyHome/report/risk/Mici';
 import {batchReport} from 'components/hoc';
-import Tabs from 'components/lib/tabs';
+import Tabs from 'antd/lib/tabs';
 const TabPane = Tabs.TabPane;
 
 @inject('routing', 'riskStore')
@@ -17,14 +17,14 @@ export default class Risk extends Component {
   render() {
     const riskStore = this.props.riskStore;
     return (
-      <Tabs>
-        <TabPane tab="法务信息">
+      <Tabs defaultActiveKey="法务信息">
+        <TabPane tab="法务信息" key="法务信息">
           <Court riskStore={riskStore}/>
         </TabPane>
-        <TabPane tab="税务公示信息">
+        <TabPane tab="税务公示信息" key="税务公示信息">
           <Tax riskStore={riskStore}/>
         </TabPane>
-        <TabPane tab="工商公示信息">
+        <TabPane tab="工商公示信息" key="工商公示信息">
           <Mici riskStore={riskStore}/>
         </TabPane>
       </Tabs>
