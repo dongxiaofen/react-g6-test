6733ba22baafd1b208ac28a30d3d6d37b29970a9
dengfuqiu <kmdengfuqiu@126.com>
2017-04-28 10:25:16 +0800

    上市brief完成，并修改kvTable的错误信息类型为文字提示

---
+++ src/components/companyHome/report/stock/Brief/index.js
@@ -0,0 +1,39 @@
+import React, {PropTypes} from 'react';
+import { observer } from 'mobx-react';
+
+import { ModuleTitle, KvTable } from 'components/common/report';
+function Brief({ brief, isEmptyObject, isLoading }) {
+  console.log(brief, '--------------brief');
+  const data = {
+    meta: {
+      items: brief,
+      dict: 'brief',
+      body: [
+        [{ 'key': 'stockCode', 'type': 'half' }, { 'key': 'company_name', 'type': 'half' }],
+        [{ 'key': 'company_english_name', 'type': 'half' }, { 'key': 'legal_person', 'type': 'half' }],
+        [{ 'key': 'secretary', 'type': 'half' }, { 'key': 'primary_industry', 'type': 'half' }],
+        [{ 'key': 'issued_price', 'type': 'half' }, { 'key': 'issued_shares', 'type': 'half' }],
+        [{ 'key': 'pe', 'type': 'half' }, { 'key': 'reg_cap', 'type': 'half' }],
+        [{ 'key': 'listing_date', 'type': 'half' }, { 'key': 'prospectus_date', 'type': 'half' }],
+        [{ 'key': 'fax', 'type': 'half' }, { 'key': 'zipcode', 'type': 'half' }],
+        [{ 'key': 'website', 'type': 'half' }, { 'key': 'reg_address', 'type': 'half' }],
+        [{ 'key': 'release_mode', 'type': 'half' }, { 'key': 'sponsor_institution', 'type': 'half' }],
+        [{ 'key': 'listing_recommender', 'type': 'half' }, { 'key': 'main_underwriter', 'type': 'half' }],
+      ],
+    },
+    isLoading: isLoading,
+    module: '公司概况',
+    error: isEmptyObject(brief)
+  };
+  return (
+    <div>
+      <ModuleTitle module="公司概况" />
+      <KvTable {...data} />
+    </div>
+  );
+}
+
+Brief.propTypes = {
+  brief: PropTypes.object,
+};
+export default observer(Brief);
--- src/containers/report/stock/Stock.js
+++ src/containers/report/stock/Stock.js
@@ -5,6 +5,8 @@ import Tabs from 'antd/lib/tabs';
 const TabPane = Tabs.TabPane;
 import { batchReport } from 'components/hoc';
 
+import Brief from 'components/companyHome/report/stock/Brief';
+
 @inject('stockStore', 'routing')
 @batchReport('stock')
 @observer
@@ -13,10 +15,11 @@ export default class Stock extends Component {
     stockStore: PropTypes.object,
   }
   render() {
+    const stockStore = this.props.stockStore;
     return (
       <Tabs defaultActiveKey="公司概况">
         <TabPane tab="公司概况" key="公司概况">
-          this is 公司概况
+          <Brief brief={stockStore.brief} isEmptyObject={stockStore.isEmptyObject} />
         </TabPane>
         <TabPane tab="公告列表" key="公告列表">
           this is 公司概况
