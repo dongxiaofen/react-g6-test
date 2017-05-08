616d25001301b93f524b70ed7b6e870cd11e33ab
dengfuqiu <kmdengfuqiu@126.com>
2017-05-08 09:09:36 +0800

    修改搜索框样式

--- src/components/reportManage/Filter/index.js
+++ src/components/reportManage/Filter/index.js
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react';
 import { observer } from 'mobx-react';
 import styles from './index.less';
-import Input from 'components/lib/input';
 
 function Filter({ reportManageStore, reportManagePager, status }) {
   const setFocus = (bool) => {
@@ -31,15 +30,17 @@ function Filter({ reportManageStore, reportManagePager, status }) {
     }
   };
   return (
-    <div className={`clearfix ${styles.wrap} ${reportManageStore.focus ? styles.Active : ''}`}>
-      <Input className={styles.searchInput}
-             onKeyUp={keyUpEvent}
-             onChange={inputName}
-             onFocus={setFocus.bind(this, true)}
-             onBlur={setFocus.bind(this, false)}
-             value={reportManageStore.companyName}
-             placeholder="输入企业名称" />
-      <i className={`fa fa-search ${styles.searchIcon}`} onClick={searchCompany}></i>
+    <div className={styles.search}>
+      {/* <div className={`clearfix ${styles.wrap} ${reportManageStore.focus ? styles.Active : ''}`}>
+    </div> */}
+      <i className="fa fa-search"></i>
+      <input className={styles.input}
+        onKeyUp={keyUpEvent}
+        onChange={inputName}
+        onFocus={setFocus.bind(this, true)}
+        onBlur={setFocus.bind(this, false)}
+        value={reportManageStore.companyName}
+        placeholder="输入企业名称" />
     </div>
   );
 }
