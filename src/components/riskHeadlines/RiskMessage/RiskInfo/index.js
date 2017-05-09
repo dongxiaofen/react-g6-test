402781c3cc691d47d9a0fc20f7f41afe7934421b
万俐君 <lijun.wan@socialcredits.cn>
2017-05-09 09:48:04 +0800

    失信被执行人

--- src/components/riskHeadlines/RiskMessage/RiskInfo/index.js
+++ src/components/riskHeadlines/RiskMessage/RiskInfo/index.js
@@ -8,7 +8,7 @@ function RiskInfo({riskHeadlinesStore, routing}) {
   const isSubCom = events.companyType === 'SUB';
   const monitorId = events.info.monitorId;
   const viewReport = ()=> {
-    routing.history.push(`/companyHome?monitorId=${monitorId}&companyType=${isSubCom ? 'ASSOCIATE' : 'MAIN'}`);
+    routing.history.push(`/companyHome?monitorId=${monitorId}&companyType=${isSubCom ? 'ASSOCIATE' : 'MAIN'}s`);
   };
   if (this.props.riskHeadlinesStore.events.info.error) {
     return <div></div>;
