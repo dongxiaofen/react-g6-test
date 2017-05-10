c55761d1215b7ca9d6d412e4d3297980f71c8fed
dengfuqiu <kmdengfuqiu@126.com>
2017-05-10 11:08:56 +0800

    删除bannerinfo 刷新报告弹窗的提示文案

--- src/components/companyHome/Banner/ReportAction/RefreshHighOrDeep/index.js
+++ src/components/companyHome/Banner/ReportAction/RefreshHighOrDeep/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import styles from './index.less';
+// import styles from './index.less';
 
 function RefreshHighOrDeep() {
   return (
-    <div className={styles.text}>
-      确认刷新报告
+    <div>
     </div>
   );
 }
