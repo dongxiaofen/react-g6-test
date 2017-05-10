08f124a747fff7f7ad2eb1c5600793ed65d9ed69
谢明 <ming.xie@socialcredits.cn>
2017-05-10 19:16:42 +0800

    修改个人核查详情身份证号码显示

--- src/components/companyHome/report/assets/bidding/Patent/index.js
+++ src/components/companyHome/report/assets/bidding/Patent/index.js
@@ -5,7 +5,7 @@ import { CardTable, ModuleTitle } from 'components/common/report/';
 function Patent({patentItemList, isLoading}) {
   const cname = (value) => {
     if (value) {
-      return value.classificationNumber.cname || value.classificationNumber.cname.length === 0 ?
+      return value.classificationNumber.cname.length === 0 ?
         '--' :
         value.classificationNumber.cname;
     }
