51ce813caf1b7d452fdc40e0855221dabaf3ee09
万俐君 <lijun.wan@socialcredits.cn>
2017-05-11 15:26:17 +0800

    经营变更转html

--- src/components/companyHome/report/AlertAnalysis/detail/Content/module/ScopeAlter/index.js
+++ src/components/companyHome/report/AlertAnalysis/detail/Content/module/ScopeAlter/index.js
@@ -4,13 +4,16 @@ import SimpleCard from 'components/common/report/alertAnalysis/SimpleCard';
 function ScopeAlter({data, type}) {
   const itemData = type === 'RULE' ? data.content : data.detail[0];
   itemData.altDate = itemData.eventTime || itemData.altDate;
+  const modifyHtml = (value) => {
+    return <span dangerouslySetInnerHTML={{__html: value || '--'}} />;
+  };
   const meta = {
     dict: 'alterList',
     body: [
       {'key': 'altItem', 'width': '12'},
       {'key': 'altDate', 'width': '12'},
-      {'key': 'altAf', 'width': '12'},
-      {'key': 'altBe', 'width': '12'}
+      {'key': 'altAf', 'width': '12', modifyBlock: modifyHtml},
+      {'key': 'altBe', 'width': '12', modifyBlock: modifyHtml}
     ],
     item: type === 'RULE' ? data.content : data.detail[0],
   };
