d2813ae4ba1ac31d0e540b27feb960dab3279d7e
changhua.li <changhua.li@socialcredits.cn>
2017-05-15 17:05:11 +0800

    账号设置--预警企业--跳转测试完成

--- src/components/accountSetting/AccountTabs/AlertCorp/index.js
+++ src/components/accountSetting/AccountTabs/AlertCorp/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
+import { browserHistory } from 'react-router';
 import { loadingComp } from 'components/hoc';
 import styles from './index.less';
-function AlertCorp({accountSettingStore}) {
+function AlertCorp({accountSettingStore, searchCompanyStore}) {
   const moduleData = accountSettingStore.tabs.alertCorp;
   const data = moduleData.content;
+  const routeToSearch = (name) => {
+    searchCompanyStore.searchTabClick('COMPANY_NAME');
+    searchCompanyStore.searchChange({target: {value: name}});
+    searchCompanyStore.getCompanyList();
+    browserHistory.push(`/searchCompany`);
+  };
   return (
     <div>
       {
         data.map((item, idx) => {
           return (
             <div key={idx} className={styles.itemBox}>
-              <span className={styles.corpName}>么么么么有限公司</span>
-              <span className={styles.score}>综合分 98</span>
-              <span className={styles.date}>最新预警日期：2012-01-01</span>
+              <span className={styles.corpName} onClick={routeToSearch.bind(null, item.companyName)}>{item.companyName}</span>
+              <span className={styles.score}>{`综合分 ${item.score}`}</span>
+              <span className={styles.date}>{`最新预警日期：${item.latestDt}`}</span>
             </div>
           );
         })
@@ -21,7 +28,7 @@ function AlertCorp({accountSettingStore}) {
     </div>
   );
 }
-export default loadingComp({
+export default inject('searchCompanyStore')(loadingComp({
   mapDataToProps: props => ({
     loading: props.accountSettingStore.tabs.alertCorp.content === undefined ? true : false,
     error: props.accountSettingStore.tabs.alertCorp.error,
@@ -29,4 +36,4 @@ export default loadingComp({
     errCategory: 1,
     height: 200,
   }),
-})(observer(AlertCorp));
+})(observer(AlertCorp)));
