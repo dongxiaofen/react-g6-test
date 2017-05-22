9f9d9f9ba02d54a3f03e15f22e34d4dcd6f84bc6
dengfuqiu <kmdengfuqiu@126.com>
2017-05-22 09:35:29 +0800

    本地资产中的资产类型和转让机构换成了radio

--- src/components/assetTransaction/AssetLocal/index.js
+++ src/components/assetTransaction/AssetLocal/index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { observer, inject } from 'mobx-react';
 import { toJS } from 'mobx';
+import Radio from 'antd/lib/radio';
 
 import cityName from 'helpers/cityName';
 import styles from './index.less';
@@ -8,6 +9,7 @@ import Select from 'components/lib/Select';
 import CardList from './CardList';
 
 const Option = Select.Option;
+const RadioGroup = Radio.Group;
 @inject('assetTransactionStore', 'uiStore', 'detailModalStore')
 @observer
 export default class AssetLocal extends Component {
@@ -138,9 +140,44 @@ export default class AssetLocal extends Component {
     this.filterHandle();
   }
 
+  radioOnChange = (key, evt) => {
+    const setParams = this.props.assetTransactionStore.setAssetLocalParams;
+    setParams(key, evt.target.value);
+    this.filterHandle();
+  };
+
+  assignorRadioDisable() {
+    const assetType = this.props.assetTransactionStore.assetLocalParams.assetType;
+    if (assetType === '拍卖资产') {
+      return true;
+    }
+    return false;
+  }
+
   render() {
     const params = this.props.assetTransactionStore.assetLocalParams;
     const assets = params.assetGt || params.assetLt ? `${params.assetGt},${params.assetLt}` : '';
+    const assetRadioOption = [
+      { label: '全部', value: '' },
+      { label: '交易资产', value: '交易资产' },
+      { label: '招商资产', value: '招商资产' },
+      { label: '拍卖资产', value: '拍卖资产', disabled: params.assignorType ? true : false },
+    ];
+    const assignorRadioOption = [
+      { label: '全部', value: '', disabled: this.assignorRadioDisable() },
+      { label: '资产管理公司', value: '资产管理公司', disabled: this.assignorRadioDisable() },
+      { label: '投资公司', value: '投资公司', disabled: this.assignorRadioDisable() },
+      { label: '银行', value: '银行', disabled: this.assignorRadioDisable() },
+      { label: 'P2P金融', value: 'P2P金融', disabled: this.assignorRadioDisable() },
+      { label: '非金融企业', value: '非金融企业', disabled: this.assignorRadioDisable() },
+      { label: '小额贷款', value: '小额贷款', disabled: this.assignorRadioDisable() },
+      { label: '保理', value: '保理', disabled: this.assignorRadioDisable() },
+      { label: '金融租赁', value: '金融租赁', disabled: this.assignorRadioDisable() },
+      { label: '保险', value: '保险', disabled: this.assignorRadioDisable() },
+      { label: '信托', value: '信托', disabled: this.assignorRadioDisable() },
+      { label: '互联网平台', value: '互联网平台', disabled: this.assignorRadioDisable() },
+      { label: '其他', value: '其他', disabled: this.assignorRadioDisable() },
+    ];
     return (
       <div>
         <div className="clearfix">
@@ -156,18 +193,6 @@ export default class AssetLocal extends Component {
             </div>
           </div>
           <div className={`clearfix ${styles.assetLocalSwitchData}`}>
-            <span className={styles.label}>转让机构</span>
-            <div className={styles.select}>
-              <Select
-                width="120px"
-                value={params.assignorType}
-                disabledStyle={params.assetType === '拍卖资产' ? true : false}
-                onChange={this.changeSelect.bind(this, 'assignorType')}>
-                {this.assignorSelectList()}
-              </Select>
-            </div>
-          </div>
-          <div className={`clearfix ${styles.assetLocalSwitchData}`}>
             <span className={styles.label}>资产金额</span>
             <div className={styles.select}>
               <Select
@@ -178,16 +203,45 @@ export default class AssetLocal extends Component {
               </Select>
             </div>
           </div>
-          <div className={`clearfix ${styles.assetLocalSwitchData}`}>
+          {/* <div className={`clearfix ${styles.assetLocalSwitchData}`}>
+            <span className={styles.label}>转让机构</span>
+            <div className={styles.select}>
+              <Select
+                width="120px"
+                value={params.assignorType}
+                onChange={this.changeSelect.bind(this, 'assignorType')}>
+                {this.assignorSelectList()}
+              </Select>
+            </div>
+          </div> */}
+          {/* <div className={`clearfix ${styles.assetLocalSwitchData}`}>
             <span className={styles.label}>资产类型</span>
             <div className={styles.select}>
               <Select
                 width="120px"
                 value={params.assetType}
                 onChange={this.changeSelect.bind(this, 'assetType')}>
-                {this.typeSelectList(params)}
+                {this.typeSelectList()}
               </Select>
             </div>
+          </div> */}
+        </div>
+        <div className={`clearfix ${styles.radiosBox}`}>
+          <span className={styles.label}>资产类型</span>
+          <div className={styles.radios}>
+            <RadioGroup
+              options={assetRadioOption}
+              value={params.assetType}
+              onChange={this.radioOnChange.bind(null, 'assetType')}/>
+          </div>
+        </div>
+        <div className={`clearfix ${styles.radiosBox}`}>
+          <span className={styles.label}>转让机构</span>
+          <div className={styles.radios}>
+            <RadioGroup
+              options={assignorRadioOption}
+              value={params.assignorType}
+              onChange={this.radioOnChange.bind(null, 'assignorType')}/>
           </div>
         </div>
         <div className={styles.cardList}>
