351a98050419333eadda6472b7c7fe71d3f18c8d
xiaofen <xiaofen.dong@socialcredits.cn>
2017-05-25 17:16:05 +0800

    预警分析－详情内容有div处理

--- src/components/companyHome/report/AlertAnalysis/TableList/TableRow.js
+++ src/components/companyHome/report/AlertAnalysis/TableList/TableRow.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer, inject } from 'mobx-react';
 import styles from './index.less';
 function TableRow({data, routing, alertAnalysisStore, networkStore}) {
+  console.log(data, 'data------------');
   const loadingId = alertAnalysisStore.loadingId;
   const viewDetail = () => {
     if (loadingId === data.id) {
