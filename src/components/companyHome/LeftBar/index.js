e53f5a2fbbd34e504dc2f93bebfaaf2b00045949
万俐君 <lijun.wan@socialcredits.cn>
2017-06-06 11:40:21 +0800

    导航基础样式

--- src/components/companyHome/LeftBar/index.js
+++ src/components/companyHome/LeftBar/index.js
@@ -3,6 +3,7 @@ import { observer, inject } from 'mobx-react';
 import { runInAction } from 'mobx';
 import styles from './index.less';
 import Tooltip from 'antd/lib/tooltip';
+import Button from 'components/lib/button';
 function LeftBar({ leftBarStore, bannerStore, routing }) {
   const activeMenu = leftBarStore.activeMenu;
   const stockCode = bannerStore.stockCode;
@@ -28,50 +29,81 @@ function LeftBar({ leftBarStore, bannerStore, routing }) {
       query: routing.location.query,
     });
   };
-  const geneBar = () => {
+  const geneBar = (type) => {
     const menuRow = [];
-    const reportType = leftBarStore.getReportType(routing);
-    barConf.forEach((menuObj) => {
-      const accessMenu = menuObj.contain.includes(reportType);
+    // onst reportType = leftBarStore.getReportType(routing);
+    barConf[type].forEach((menuObj) => {
+      const accessMenu = true;
       const arrowCss = activeMenu.includes(menuObj.menuKey) ? styles.arrow + ` ${styles.arrowAnim}` : styles.arrow;
       const menuCss = accessMenu ? styles.menuCss : styles.menuDisCss;
       const itemRow = [];
-      menuRow.push(
-        <Tooltip key={menuObj.menuText} title={accessMenu ? '' : menuObj.helpInfo} placement="right">
+      if (menuObj.children) {
+        menuRow.push(
           <div
+            key={menuObj.menuKey}
             className={menuCss}
             onClick={changeMenu.bind(this, menuObj.menuKey, accessMenu)}
           >
             {menuObj.menuText}
             <i className={`fa fa-angle-down ${arrowCss}`}></i>
           </div>
-        </Tooltip>
-      );
-      menuObj.children.forEach((itemObj, itemIdx) => {
-        const accessItem = itemObj.contain.includes(reportType);
-        let itemCss = leftBarStore.activeItem === itemObj.menuKey ? styles.itemActCss : styles.itemCss;
-        itemCss = accessItem ? itemCss : styles.itemDisCss;
-        itemCss = activeMenu.includes(menuObj.menuKey) ? itemCss : styles.hide;
-        if (itemObj.menuKey !== 'stock' || stockCode) {
-          itemRow.push(
-            <Tooltip key={itemObj.menuText + itemIdx} title={accessItem ? '' : itemObj.helpInfo} placement="right">
-              <div
-                className={itemCss}
-                onClick={changeItem.bind(this, itemObj.menuKey, accessItem)}
-              >
-                {itemObj.menuText}
-              </div>
-            </Tooltip>
-          );
-        }
-      });
+        );
+        menuObj.children.forEach((itemObj, itemIdx) => {
+          const accessItem = true;
+          let itemCss = leftBarStore.activeItem === itemObj.menuKey ? styles.itemActCss : styles.itemCss;
+          itemCss = accessItem ? itemCss : styles.itemDisCss;
+          itemCss = activeMenu.includes(menuObj.menuKey) ? itemCss : styles.hide;
+          if (itemObj.menuKey !== 'stock' || stockCode) {
+            itemRow.push(
+              <Tooltip key={itemObj.menuText + itemIdx} title={accessItem ? '' : itemObj.helpInfo} placement="right">
+                <div
+                  className={itemCss}
+                  onClick={changeItem.bind(this, itemObj.menuKey, accessItem)}
+                >
+                  {itemObj.menuText}
+                </div>
+              </Tooltip>
+            );
+          }
+        });
+      } else {
+        menuRow.push(
+          <div
+            key={menuObj.menuKey}
+            className={menuCss}
+            onClick={changeItem.bind(this, menuObj.menuKey, true)}
+          >
+            {menuObj.menuText}
+          </div>
+        );
+      }
       menuRow.push(itemRow);
     });
     return menuRow;
   };
   return (
-    <div className={styles.wrap}>
-      {geneBar()}
+    <div>
+      <div className={styles.wrap}>
+        <div className={`${styles.title} clearfix`}>
+          <p className={styles.reportType}>贷前基础报告</p>
+          <Button btnType="primary" className={styles.btnReport}>升级报告</Button>
+        </div>
+        {geneBar('report')}
+      </div>
+      <div className={styles.wrap}>
+        <div className={`${styles.title} clearfix`}>
+          <p className={styles.reportType}>贷中分析</p>
+          <Button btnType="primary" className={styles.btnMonitor}>创建分析</Button>
+        </div>
+        {geneBar('loaning')}
+      </div>
+      <div className={styles.wrap}>
+        <div className={`${styles.title} clearfix`}>
+          <p className={styles.reportType}>贷后监控</p>
+          <Button btnType="primary" className={styles.btnMonitor}>加入监控</Button>
+        </div>
+        {geneBar('monitor')}
+      </div>
     </div>
   );
 }
--- src/stores/leftBar.js
+++ src/stores/leftBar.js
@@ -1,6 +1,6 @@
-import { observable, action, reaction, runInAction } from 'mobx';
-const helpInfo1 = '创建监控后，可查看该板块信息';
-const helpInfo2 = '创建监控、报告后，可查看该板块信息';
+import { observable, action, reaction } from 'mobx';
+// const helpInfo1 = '创建监控后，可查看该板块信息';
+// const helpInfo2 = '创建监控、报告后，可查看该板块信息';
 class LeftBarStore {
   constructor() {
     reaction(
@@ -12,184 +12,138 @@ class LeftBarStore {
   }
   @observable activeMenu = [];
   @observable activeItem = '';
-  barConf = [
-    {
-      menuText: '基本信息',
-      menuKey: 'report',
-      children: [
-        {
-          menuText: '工商信息',
-          menuKey: 'corpDetail',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '对外投资任职',
-          menuKey: 'investAndOffice',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '上市披露',
-          menuKey: 'stock',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '税务信息',
-          menuKey: 'tax',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-        {
-          menuText: '风险信息',
-          menuKey: 'risk',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '新闻信息',
-          menuKey: 'internet',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '经营信息',
-          menuKey: 'assets',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '团队信息',
-          menuKey: 'team',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-      ],
-      helpInfo: helpInfo2,
-      contain: ['monitor', 'report'],
-    },
-    {
-      menuText: '关联网络',
-      menuKey: 'network',
-      children: [
-        // {
-        //   menuText: '全网关系',
-        //   menuKey: 'forceNetwork',
-        //   helpInfo: helpInfo1,
-        //   contain: ['deepMonitor', 'monitor']
-        // },
-        {
-          menuText: '关联关系',
-          menuKey: 'network',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '风险关系', menuKey: 'blackNetwork',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-      ],
-      helpInfo: helpInfo2,
-      contain: ['monitor', 'report'],
-    },
-    {
-      menuText: '趋势分析',
-      menuKey: 'trendAnalyse',
-      children: [
-        {
-          menuText: '事件时间轴',
-          menuKey: 'timeAxis',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-        {
-          menuText: '预警分析',
-          menuKey: 'alertAnalysis',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-      ],
-      helpInfo: helpInfo1,
-      contain: ['monitor'],
-    },
-    {
-      menuText: '信息核查',
-      menuKey: 'infoCheck',
-      children: [
-        {
-          menuText: '关联人核查',
-          menuKey: 'relPerCheck',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-        {
-          menuText: '税务核查',
-          menuKey: 'taxCheck',
-          helpInfo: helpInfo2,
-          contain: ['monitor', 'report']
-        },
-      ],
-      helpInfo: helpInfo2,
-      contain: ['monitor', 'report'],
-    },
-    {
-      menuText: '企业现勘',
-      menuKey: 'corprationXk',
-      children: [
-        {
-          menuText: '现勘记录',
-          menuKey: 'nowRecord',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-      ],
-      helpInfo: helpInfo1,
-      contain: ['monitor'],
-    },
-    {
-      menuText: '贷中分析',
-      menuKey: 'loaning',
-      children: [
-        {
-          menuText: '多维综合评价',
-          menuKey: 'comprehenEval',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-        {
-          menuText: '盈利能力分析',
-          menuKey: 'profitEval',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-        {
-          menuText: '营运能力分析',
-          menuKey: 'operationEval',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-        {
-          menuText: '成长能力分析',
-          menuKey: 'growthAbilityEval',
-          helpInfo: helpInfo1,
-          contain: ['monitor']
-        },
-      ],
-      helpInfo: helpInfo1,
-      contain: ['monitor'],
-    },
-  ];
+  barConf = {
+    'report': [
+      {
+        menuText: '基本信息',
+        menuKey: 'report',
+        children: [
+          {
+            menuText: '工商信息',
+            menuKey: 'corpDetail',
+          },
+          {
+            menuText: '对外投资任职',
+            menuKey: 'investAndOffice',
+          },
+          {
+            menuText: '上市披露',
+            menuKey: 'stock',
+          },
+          {
+            menuText: '税务信息',
+            menuKey: 'tax',
+          },
+          {
+            menuText: '风险信息',
+            menuKey: 'risk',
+          },
+          {
+            menuText: '新闻信息',
+            menuKey: 'internet',
+          },
+          {
+            menuText: '经营信息',
+            menuKey: 'assets',
+          },
+          {
+            menuText: '团队信息',
+            menuKey: 'team',
+          },
+        ],
+      },
+      {
+        menuText: '关联网络',
+        menuKey: 'network',
+        children: [
+          {
+            menuText: '关联关系',
+            menuKey: 'network',
+          },
+          {
+            menuText: '风险关系', menuKey: 'blackNetwork',
+          },
+        ],
+      },
+      {
+        menuText: '趋势分析',
+        menuKey: 'trendAnalyse',
+        children: [
+          {
+            menuText: '事件时间轴',
+            menuKey: 'timeAxis',
+          },
+          {
+            menuText: '预警分析',
+            menuKey: 'alertAnalysis',
+          },
+        ],
+      },
+      {
+        menuText: '信息核查',
+        menuKey: 'infoCheck',
+        children: [
+          {
+            menuText: '关联人核查',
+            menuKey: 'relPerCheck',
+          },
+          {
+            menuText: '税务核查',
+            menuKey: 'taxCheck',
+          },
+        ],
+      },
+      {
+        menuText: '企业现勘',
+        menuKey: 'corprationXk',
+        children: [
+          {
+            menuText: '现勘记录',
+            menuKey: 'nowRecord',
+          },
+        ],
+      },
+    ],
+    loaning: [
+      {
+        menuText: '多维综合评价',
+        menuKey: 'comprehenEval',
+      },
+      {
+        menuText: '盈利能力分析',
+        menuKey: 'profitEval',
+      },
+      {
+        menuText: '营运能力分析',
+        menuKey: 'operationEval',
+      },
+      {
+        menuText: '成长能力分析',
+        menuKey: 'growthAbilityEval',
+      },
+    ],
+    monitor: [
+      {
+        menuText: '监控时间轴',
+        menuKey: 'monitorTimeAxis',
+      },
+      {
+        menuText: '监控预警',
+        menuKey: 'monitorAlert',
+      }
+    ]
+  }
 
   @action.bound setMenuByItem() {
     const barConf = this.barConf;
-    barConf.forEach(item => {
-      item.children.forEach(child => {
-        if (child.menuKey === this.activeItem) {
-          runInAction('切换报告runInAction一级目录', () => {
-            if (!this.activeMenu.includes(item.menuKey)) {
-              this.activeMenu.push(item.menuKey);
+    Object.keys(barConf).forEach((moduleKey)=>{
+      barConf[moduleKey].forEach(item => {
+        if (item.children) {
+          item.children.forEach(child => {
+            if (child.menuKey === this.activeItem) {
+              if (!this.activeMenu.includes(item.menuKey)) {
+                this.activeMenu.push(item.menuKey);
+              }
             }
           });
         }
