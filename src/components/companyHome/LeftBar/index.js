e86318ceb1b0584e81d1939751d3eebb61be6b5c
luoyang <yang.luo@socialcredits.cn>
2017-06-08 16:40:15 +0800

    增加现勘链接

--- src/components/companyHome/LeftBar/index.js
+++ src/components/companyHome/LeftBar/index.js
@@ -105,6 +105,13 @@ function LeftBar({ leftBarStore, bannerStore, routing, companyHomeStore}) {
   };
   const companyName = routing.location.query.companyName;
   const reportTitle = companyHomeStore.reportInfo.reportId !== '' ? '贷前高级报告' : '贷前基础报告';
+  // 现勘记录路由
+  const changeNowRecord = () => {
+    routing.push({
+      pathname: `/companyHome/nowRecord`,
+      query: routing.location.query,
+    });
+  };
   return (
     <div>
       <div className={styles.wrap}>
@@ -150,7 +157,9 @@ function LeftBar({ leftBarStore, bannerStore, routing, companyHomeStore}) {
         </div>
         {geneBar('monitor')}
       </div>
-      <div className={`${styles.wrap} ${styles.recordWrap}`}>
+      <div
+        onClick={changeNowRecord}
+        className={`${styles.wrap} ${styles.recordWrap}`}>
         <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
         <span className={styles.record}>调查记录</span>
       </div>
