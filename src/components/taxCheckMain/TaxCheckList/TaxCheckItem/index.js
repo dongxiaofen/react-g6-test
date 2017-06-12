529a7255b71cb6c10a0b75f58edf2a1a57b2360c
冯维 <wei.feng@socialcreidts.cn>
2017-06-12 20:30:16 +0800

    按查询时间分裂表格

--- src/components/taxCheckMain/TaxCheckList/TaxCheckItem/index.js
+++ src/components/taxCheckMain/TaxCheckList/TaxCheckItem/index.js
@@ -6,40 +6,63 @@ import styles from './index.less';
 
 function TaxCheckItem({taxCheckStore}) {
   const dataList = taxCheckStore.taxCheckInfo.content;
-  const listDom = [];
+  const listGroup = [];
   if (dataList && dataList.length > 0) {
-    dataList.map((obj, idx)=>{
-      listDom.push(
-        <tr
-          key={`${idx}checkList`}
-          className={styles.content}>
-          <td>{obj.year}年</td>
-          <td>{obj.taxIndex}</td>
-          <td className={obj.match ? '' : styles.errorColor}>{obj.input}元</td>
-          <td className={obj.match ? '' : styles.errorColor}>{obj.match ? '匹配' : '不匹配'}</td>
-          <td>{obj.checkTs}</td>
-        </tr>
+    let lastTime = '';
+    let group = [];
+    dataList.map((item) => {
+      if (item.checkTs === lastTime) {
+        group.push(item);
+      } else {
+        if (group.length > 0 ) {
+          listGroup.push(group);
+        }
+        group = [];
+        lastTime = item.checkTs;
+        group.push(item);
+      }
+    });
+    if (group.length > 0 ) {
+      listGroup.push(group);
+    }
+  }
+  console.log(listGroup);
+  const tableDoms = [];
+  if (listGroup.length > 0) {
+    listGroup.map((item, _idx) => {
+      const tableBody = [];
+      item.map((obj, idx) => {
+        tableBody.push(
+          <tr
+            key={`${idx}checkListRow`}
+            className={styles.content}>
+            <td>{obj.year}年</td>
+            <td>{obj.taxIndex}</td>
+            <td className={obj.match ? '' : styles.errorColor}>{obj.input}元</td>
+            <td className={obj.match ? '' : styles.errorColor}>{obj.match ? '匹配' : '不匹配'}</td>
+            {idx === 0 ? <td rowSpan="1000">{obj.checkTs}</td> : null}
+          </tr>
+        );
+      });
+      const tableDom = (
+        <table className={styles.table}>
+          <tbody key={`${_idx}checkListTbale`}>
+            <tr className={styles.title}><td>年度</td><td>指标</td><td>核查金额</td><td>与实际金额核查结果</td><td>核查时间</td></tr>
+            {tableBody}
+          </tbody>
+        </table>
       );
+      tableDoms.push(tableDom);
     });
   }
+
   return (
     <div className={styles.box}>
       <div className={styles.companyName}>{taxCheckStore.taxCheckInfoCompany}</div>
       <span className={styles.text}>
         核查结果提示：<span>税务核查金额和实际金额误差在5%以内时即“匹配”，超过5%即“不匹配”</span>
       </span>
-      <table className={styles.table}>
-        <tbody>
-          <tr className={styles.title}>
-            <td>年度</td>
-            <td>指标</td>
-            <td>核查金额</td>
-            <td>与实际金额核查结果</td>
-            <td>核查时间</td>
-          </tr>
-          {listDom}
-        </tbody>
-      </table>
+      {tableDoms}
       {/* <div className={styles.page}>
         <Pager tData={taxCheckStore.taxListData.content} module="taxCheckPager"
                uiStore={uiStore} type="large"/>
