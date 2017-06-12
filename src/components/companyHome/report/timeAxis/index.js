1ea207c49fdbfb2fc67c807f87c0a990081b5b34
changhua.li <changhua.li@socialcredits.cn>
2017-06-12 17:24:04 +0800

    时间轴样式调整

--- src/components/companyHome/report/timeAxis/index.js
+++ src/components/companyHome/report/timeAxis/index.js
@@ -5,12 +5,11 @@ import Axis from './Axis';
 import Event from './Event';
 
 function TimeAxis(props) {
-  const {time, module, relation} = props.timeAxisStore.eventParams;
   return (
     <div style={{margin: '-5px 10px 0 10px'}}>
       <ModuleTitle module="时间轴" />
       <Axis {...props} />
-      <ModuleTitle module={time ? `${time}事件（${module} - ${relation}）` : '事件'} />
+      <ModuleTitle module="时间轴事件详情" />
       <Event {...props} />
     </div>
   );
