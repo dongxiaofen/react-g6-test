636fc7b20d75ed55ee8c5404117752d5b21481e4
changhua.li <changhua.li@socialcredits.cn>
2017-06-14 18:26:25 +0800

    监控列表卸载时移除所有状态

--- src/containers/companyHome/CompanyHome.js
+++ src/containers/companyHome/CompanyHome.js
@@ -4,7 +4,7 @@ import { runInAction } from 'mobx';
 import Banner from 'components/companyHome/Banner';
 import LeftBar from 'components/companyHome/LeftBar';
 import { Container, Row, Col } from 'components/common/layout';
-import CirclesLoading from 'components/common/CirclesLoading';
+import BarLoading from 'components/common/BarLoading';
 import styles from './index.less';
 
 @inject(
@@ -127,7 +127,7 @@ export default class CompanyHome extends Component {
       return !this.props.companyHomeStore.reportInfo[key];
     });
     if (noReport) {
-      return <CirclesLoading />;
+      return <BarLoading />;
     }
     return (
       <Container id="reportContainer">
