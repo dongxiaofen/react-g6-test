84e3e814f57cdab1686308c419b1e6222247174f
changhua.li <changhua.li@socialcredits.cn>
2017-06-15 14:49:06 +0800

    移除时间轴和监控预警边距

--- src/components/companyHome/report/timeAxis/index.js
+++ src/components/companyHome/report/timeAxis/index.js
@@ -6,7 +6,7 @@ import Event from './Event';
 
 function TimeAxis(props) {
   return (
-    <div style={{margin: '-5px 10px 0 10px'}}>
+    <div>
       <ModuleTitle module={props.title} />
       <Axis {...props} />
       <ModuleTitle module="时间轴事件详情" />
