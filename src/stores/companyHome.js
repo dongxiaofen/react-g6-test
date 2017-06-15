15b2ebae1aaff35ae3323a25dba83d3946368ef5
万俐君 <lijun.wan@socialcredits.cn>
2017-06-15 11:40:23 +0800

    fix创建分析提示文案

--- src/stores/companyHome.js
+++ src/stores/companyHome.js
@@ -23,7 +23,7 @@ class CompanyHomeStore {
     { label: '多维综合评价', value: 'SCORE', checked: true},
     { label: '盈利能力分析', value: 'PROFIT', checked: true},
     { label: '营运能力分析', value: 'OPERATION', checked: true},
-    { label: '发展能力分析', value: 'GROWING', checked: true},
+    { label: '成长能力分析', value: 'GROWING', checked: true},
   ];
   @observable monitorTime = 1;
   @observable loanLoading = false;
@@ -95,6 +95,20 @@ class CompanyHomeStore {
     };
     modalStore.openCompModal({ ...args });
   }
+  getMessageText(data) {
+    const status = ['existProfitDetail', 'existOperationDetail', 'existGrowingDetail'];
+    const textConfig = ['盈利', '运营', '成长'];
+    const text = [];
+    status.forEach((item, idx)=>{
+      if (data.hasOwnProperty(item) && data[item] === false) {
+        text.push(textConfig[idx]);
+      }
+    });
+    if (text.length > 0) {
+      return text.join('、');
+    }
+    return '';
+  }
   @action.bound createLoanRep(companyName) {
     let text = {
       content: '分析报告创建成功'
@@ -103,12 +117,12 @@ class CompanyHomeStore {
     companyHomeApi.createAnalyRep({companyName, items: this.loanOptValue})
     .then(action('createAnalyRep', (resp) => {
       let options = this.loanOptValue;
-      const onlyScore = this.loanOptValue.length === 1 && this.loanOptValue[0] === 'SCORE' ? true : false;
-      if (!onlyScore && !resp.data.existTaxDetail) {
+      const msgText = this.getMessageText(resp.data);
+      if (msgText !== '') {
         const idx = this.loanOptValue.indexOf('SCORE');
         options = idx > -1 ? ['SCORE'] : [];
         text = {
-          content: '抱歉，由于企业不存在盈利、运营、成长数据，分析失败',
+          content: `抱歉，由于企业不存在${msgText}数据，分析失败`,
           duration: 3000
         };
       }
