d45e9a6c3f72edb04ebfc4b5a955a6b3a7d15096
谢明 <ming.xie@socialcredits.cn>
2017-06-16 11:40:25 +0800

    增加主页文案提示

--- src/stores/accountProfile.js
+++ src/stores/accountProfile.js
@@ -7,14 +7,14 @@ class AccountProfileStore {
   @observable subWarningStatisticsData = {};
 
 
-  @observable ownWarningCompnay = [];
-  @observable subWarningCompnay = [];
+  @observable ownWarningCompnay = {};
+  @observable subWarningCompnay = {};
 
-  @observable ownHighRisk = [];
-  @observable subHighRisk = [];
+  @observable ownHighRisk = {};
+  @observable subHighRisk = {};
 
-  @observable ownLowestScore = [];
-  @observable subLowestScore = [];
+  @observable ownLowestScore = {};
+  @observable subLowestScore = {};
 
   @observable subAccount10Data = [];
   @observable subNewestRuleData = [];
@@ -123,72 +123,72 @@ class AccountProfileStore {
   @action.bound getOwnNewest() {
     accountProfileApi.ownNewest()
       .then(action( (response) => {
-        this.ownWarningCompnay = response.data;
+        this.ownWarningCompnay = response;
         this.ownWarningCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.ownWarningCompnayIsLoading = false;
-        console.log(err);
+        this.ownWarningCompnay = err.response.data;
       }));
   }
 
   @action.bound getSubNewest() {
     accountProfileApi.subNewest()
       .then(action( (response) => {
-        this.subWarningCompnay = response.data;
+        this.subWarningCompnay = response;
         this.subWarningCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.subWarningCompnayIsLoading = false;
-        console.log(err);
+        this.subWarningCompnay = err.response.data;
       }));
   }
 
   @action.bound getOwnHightRisk() {
     accountProfileApi.ownHightRisk()
       .then(action( (response) => {
-        this.ownHighRisk = response.data;
+        this.ownHighRisk = response;
         this.ownRiskCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.ownRiskCompnayIsLoading = false;
-        console.log(err);
+        this.ownHighRisk = err.response.data;
       }));
   }
 
   @action.bound getSubHightRisk() {
     accountProfileApi.subHightRisk()
       .then(action( (response) => {
-        this.subHighRisk = response.data;
+        this.subHighRisk = response;
         this.subRiskCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.subRiskCompnayIsLoading = false;
-        console.log(err);
+        this.subHighRisk = err.response.data;
       }));
   }
 
   @action.bound getOwnLowestScore() {
     accountProfileApi.ownLowestScore()
       .then(action( (response) => {
-        this.ownLowestScore = response.data;
+        this.ownLowestScore = response;
         this.ownLowScoreCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.ownLowScoreCompnayIsLoading = false;
-        console.log(err);
+        this.ownLowestScore = err.response.data;
       }));
   }
 
   @action.bound getSubLowestScore() {
     accountProfileApi.subLowestScore()
       .then(action( (response) => {
-        this.subLowestScore = response.data;
+        this.subLowestScore = response;
         this.subLowScoreCompnayIsLoading = false;
       }))
       .catch(action( (err) => {
         this.subLowScoreCompnayIsLoading = false;
-        console.log(err);
+        this.subLowestScore = err.response.data;
       }));
   }
 
