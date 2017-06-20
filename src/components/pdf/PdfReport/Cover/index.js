310b00f4d586e6a9386a40f79b0077a5d4ae2f43
谢明 <ming.xie@socialcredits.cn>
2017-06-20 17:19:11 +0800

    加入封面

---
+++ src/components/pdf/PdfReport/Cover/index.js
@@ -0,0 +1,19 @@
+import React, {PropTypes} from 'react';
+import { observer, inject } from 'mobx-react';
+import styles from './index.less';
+
+
+function Cover({clientStore, pdfStore}) {
+  return (
+    <div className={styles.cover}>
+      <div className={`${clientStore.envConfig === 'cfca_prod' ? styles.logo_sc : styles.logo_sc}`}></div>
+      <h1>{pdfStore.reportType}</h1>
+      <h2>{pdfStore.companyName}</h2>
+    </div>
+  );
+}
+
+Cover.propTypes = {
+  foo: PropTypes.string,
+};
+export default inject('clientStore', 'pdfStore')(observer(Cover));
