75f748b101fe2719406d8f70a23dd10ac0347c2f
冯维 <wei.feng@socialcreidts.cn>
2017-06-20 14:29:03 +0800

    企业对外投资，法人对外投资任职取消tab

--- src/containers/report/entinvItem/EntinvItem.js
+++ src/containers/report/entinvItem/EntinvItem.js
@@ -1,22 +1,17 @@
 import React, { Component} from 'react';
 import { observer, inject } from 'mobx-react';
-import Tabs from 'antd/lib/tabs';
-const TabPane = Tabs.TabPane;
+// import Tabs from 'antd/lib/tabs';
+// const TabPane = Tabs.TabPane;
 import { batchReport } from 'components/hoc';
 import Enterprise from 'components/companyHome/report/investment/Enterprise';
+
 @inject('routing', 'investmentStore')
 @batchReport('investmentStore')
 @observer
 export default class EntinvItem extends Component {
   render() {
     return (
-      <div>
-        <Tabs tab="企业对外投资">
-          <TabPane tab="企业对外投资" key="企业对外投资">
-            <Enterprise {...this.props} />
-          </TabPane>
-        </Tabs>
-      </div>
+      <Enterprise {...this.props} />
     );
   }
 }
--- src/containers/report/frPosAndInv/FrPosAndInv.js
+++ src/containers/report/frPosAndInv/FrPosAndInv.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import Tabs from 'antd/lib/tabs';
-const TabPane = Tabs.TabPane;
+// import Tabs from 'antd/lib/tabs';
+// const TabPane = Tabs.TabPane;
 import { batchReport } from 'components/hoc';
 import Investment from 'components/companyHome/report/investment/Investment';
 import Office from 'components/companyHome/report/investment/Office';
@@ -13,12 +13,8 @@ export default class FrPosAndInv extends Component {
   render() {
     return (
       <div>
-        <Tabs tab="法人对外投资任职">
-          <TabPane tab="法人对外投资任职" key="法人对外投资任职">
-            <Investment {...this.props} />
-            <Office {...this.props} />
-          </TabPane>
-        </Tabs>
+        <Investment {...this.props} />
+        <Office {...this.props} />
       </div>
     );
   }
