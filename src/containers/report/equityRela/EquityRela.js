7a48554191400854c4aad8f7609fae0b14eade5b
冯维 <wei.feng@socialcreidts.cn>
2017-06-20 14:23:06 +0800

    优化抵质押tab显示

--- src/containers/report/equityRela/EquityRela.js
+++ src/containers/report/equityRela/EquityRela.js
@@ -3,43 +3,57 @@ import { observer, inject } from 'mobx-react';
 import { batchReport } from 'components/hoc';
 import Tabs from 'antd/lib/tabs';
 const TabPane = Tabs.TabPane;
+import loadingComp from 'components/hoc/LoadingComp';
 import EquityFreeze from 'components/companyHome/report/equityRela/EquityFreeze';
 import SharePledge from 'components/companyHome/report/equityRela/SharePledge';
 import EquityTransfer from 'components/companyHome/report/equityRela/EquityTransfer';
 
 @inject('routing', 'MortageStore')
 @batchReport('MortageStore')
+@loadingComp({
+  mapDataToProps: props => ({
+    loading: props.MortageStore.isLoading,
+    error: false,
+    module: '股权相关'
+  })
+})
 @observer
 export default class EquityRela extends Component {
   static propTypes = {
     MortageStore: PropTypes.object,
   }
   render() {
-    let EquityFreezeCount = 0;
-    let SharePledgeCount = 0;
-    let EquityTransferCount = 0;
     const MortageStore = this.props.MortageStore;
-    if (MortageStore.sharesFrostList.content && MortageStore.sharesFrostList.content.length > 0) {
-      EquityFreezeCount = MortageStore.sharesFrostList.content.length;
+    const {
+      sharesFrostListCount,
+      sharesImpawnListCount,
+      sharesTransferListCount
+    } = MortageStore;
+    let defaultActiveKey = '股权冻结';
+    if (sharesFrostListCount === 0) {
+      defaultActiveKey = '股权质押';
     }
-    if (MortageStore.sharesImpawnList.content && MortageStore.sharesImpawnList.content.length > 0) {
-      SharePledgeCount = MortageStore.sharesImpawnList.content.length;
+    if (sharesImpawnListCount === 0) {
+      defaultActiveKey = '股权转让';
     }
-    if (MortageStore.sharesTransferList.content && MortageStore.sharesTransferList.content.length > 0) {
-      EquityTransferCount = MortageStore.sharesTransferList.content.length;
+    if (sharesTransferListCount === 0) {
+      defaultActiveKey = '股权冻结';
     }
     return (
-      <Tabs defaultActiveKey="股权冻结">
-        <TabPane tab={`股权冻结（${EquityFreezeCount}）`}
-          key="股权冻结">
+      <Tabs defaultActiveKey={defaultActiveKey}>
+        <TabPane tab={`股权冻结（${sharesFrostListCount}）`}
+          key="股权冻结"
+          disabled={ sharesFrostListCount > 0 ? false : true}>
           <EquityFreeze {...this.props}/>
         </TabPane>
-        <TabPane tab={`股权质押（${SharePledgeCount}）`}
-          key="股权质押">
+        <TabPane tab={`股权质押（${sharesImpawnListCount}）`}
+          key="股权质押"
+          disabled={ sharesImpawnListCount > 0 ? false : true}>
           <SharePledge {...this.props}/>
         </TabPane>
-        <TabPane tab={`股权转让（${EquityTransferCount}）`}
-          key="股权转让">
+        <TabPane tab={`股权转让（${sharesTransferListCount}）`}
+          key="股权转让"
+          disabled={ sharesTransferListCount > 0 ? false : true}>
           <EquityTransfer {...this.props}/>
         </TabPane>
       </Tabs>
--- src/stores/report/mortage.js
+++ src/stores/report/mortage.js
@@ -4,12 +4,17 @@ import { companyHomeApi } from 'api';
 // import axios from 'axios';
 
 class MortageStore {
+  @observable isLoading = true;
   @observable sharesFrostList = {};
   @observable sharesImpawnList = {};
   @observable sharesTransferList = {};
+  @observable sharesFrostListCount = 0;
+  @observable sharesImpawnListCount = 0;
+  @observable sharesTransferListCount = 0;
   @action.bound getReportModule(params) {
     companyHomeApi.getReportModule('risk/pledge', params)
       .then(action('get pledge', resp => {
+        this.isLoading = false;
         const noData = {error: {message: '暂无信息'}, content: []};
         const sharesFrostListNoData = !resp.data || !resp.data.shares || !resp.data.shares.sharesFrostList || resp.data.shares.sharesFrostList.length === 0;
         const sharesImpawnListNoDate = !resp.data || !resp.data.shares || !resp.data.shares.sharesImpawnList || resp.data.shares.sharesImpawnList.length === 0;
@@ -17,10 +22,13 @@ class MortageStore {
         this.sharesFrostList = sharesFrostListNoData ? noData : {content: resp.data.shares.sharesFrostList};
         this.sharesImpawnList = sharesImpawnListNoDate ? noData : {content: resp.data.shares.sharesImpawnList};
         this.sharesTransferList = sharesTransferListNoData ? noData : {content: resp.data.shares.sharesTransferList};
-        // uiStore.uiState.taxCheckPager.totalElements = resp.data.totalElements;
+        this.sharesFrostListCount = sharesFrostListNoData ? 0 : resp.data.shares.sharesFrostList.length;
+        this.sharesImpawnListCount = sharesImpawnListNoDate ? 0 : resp.data.shares.sharesImpawnList.length;
+        this.sharesTransferListCount = sharesTransferListNoData ? 0 : resp.data.shares.sharesTransferList.length;
       }))
       .catch(action('get pledge error', err => {
         const errorData = {error: err.response.data, content: []};
+        this.isLoading = false;
         this.sharesFrostList = errorData;
         this.sharesImpawnList = errorData;
         this.sharesTransferList = errorData;
