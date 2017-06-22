e8bea87e35ac32f51b7ccc267376d11c03777571
huyao <huyao@sc.cn>
2017-06-22 15:45:06 +0800

    重复登录弹窗

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -62,7 +62,7 @@ reactTransform[1].transforms.push({
 });
 
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   context: path.resolve(__dirname, '..'),
   entry: {
     'main': [
