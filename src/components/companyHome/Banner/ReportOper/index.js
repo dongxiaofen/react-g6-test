57a1b939200291d38e6c13462202a24fedba29c4
万俐君 <lijun.wan@socialcredits.cn>
2017-06-27 14:15:11 +0800

    隐藏贷中分析

--- src/components/companyHome/Banner/ReportOper/index.js
+++ src/components/companyHome/Banner/ReportOper/index.js
@@ -24,7 +24,7 @@ function ReportOper({ companyHomeStore, routing, bannerStore }) {
           : ''
       }
       {
-        companyHomeStore.reportInfo.dimensions.length < 4 ?
+        companyHomeStore.reportInfo.dimensions.length < 1 ?
           <Button
             btnType="primary"
             className={styles.btnLoan}
