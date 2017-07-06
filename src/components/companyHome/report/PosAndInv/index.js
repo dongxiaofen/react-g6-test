300fa9abd4617b35c5acd7bfcef5af4793307a44
冯维 <wei.feng@socialcreidts.cn>
2017-07-06 19:21:35 +0800

    完善董建高loading显示

--- src/components/companyHome/report/PosAndInv/index.js
+++ src/components/companyHome/report/PosAndInv/index.js
@@ -8,7 +8,7 @@ function PosAndInv({investmentStore}) {
   const manageData = investmentStore.manageData;
   const count = manageData.length ? manageData.length : 0;
 
-  const isLoading = this.props.investmentStore.manageData === undefined ? true : false;
+  const isLoading = this.props.investmentStore.loading;
   const error = this.props.investmentStore.manageData.error;
   return (
     <div className={styles.box}>
