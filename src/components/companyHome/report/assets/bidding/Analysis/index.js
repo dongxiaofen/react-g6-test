14f214781bc5392960bb70781791e59d39fdd633
dengfuqiu <kmdengfuqiu@126.com>
2017-07-06 14:13:16 +0800

    修改招投标分析图选择年度、季度、月度之后再切换其它时间选项，echart图表残留的问题

--- src/components/companyHome/report/assets/bidding/Analysis/index.js
+++ src/components/companyHome/report/assets/bidding/Analysis/index.js
@@ -1,43 +1,54 @@
-import React, {PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import { observer } from 'mobx-react';
 import { ModuleTitle } from 'components/common/report/';
 import Chart from './Chart';
 import styles from './index.less';
 
-function Analysis({ assetsStore }) {
-  const menuActive = (text) => {
-    if (text === assetsStore.biddingAnalysisActive) {
+
+class Analysis extends Component {
+  componentDidMount() {
+    this.props.assetsStore.modifyBiddingAnalysis();
+  }
+
+
+  menuActive = (text) => {
+    if (text === this.props.assetsStore.biddingAnalysisActive) {
       return styles.menuTitleActive;
     }
     return '';
   };
 
-  const menuChange = (value) => {
-    assetsStore.updateValue('biddingAnalysisActive', value);
+  menuChange = (value) => {
+    this.props.assetsStore.updateValue('biddingAnalysisActive', value);
   };
-  return (
-    <div>
-      <div className="clearfix">
-        <div className={styles.title}>
-          <ModuleTitle module="招投标分析图" />
+
+  render() {
+    const assetsStore = this.props.assetsStore;
+    return (
+      <div>
+        <div className="clearfix">
+          <div className={styles.title}>
+            <ModuleTitle module="招投标分析图" />
+          </div>
+          {
+            !assetsStore.isErrAnalysis
+              ?
+              <div className={`cleafix ${styles.menu}`}>
+                <div className={`${styles.menuTitle} ${this.menuActive('年度')}`} onClick={this.menuChange.bind(null, '年度')}>年度</div>
+                <div className={`${styles.menuTitle} ${this.menuActive('季度')}`} onClick={this.menuChange.bind(null, '季度')}>季度</div>
+                <div className={`${styles.menuTitle} ${this.menuActive('月度')}`} onClick={this.menuChange.bind(null, '月度')}>月度</div>
+              </div>
+              : null
+          }
         </div>
-        {
-          !assetsStore.isErrAnalysis
-          ?
-            <div className={`cleafix ${styles.menu}`}>
-              <div className={`${styles.menuTitle} ${menuActive('年度')}`} onClick={menuChange.bind(null, '年度')}>年度</div>
-              <div className={`${styles.menuTitle} ${menuActive('季度')}`} onClick={menuChange.bind(null, '季度')}>季度</div>
-              <div className={`${styles.menuTitle} ${menuActive('月度')}`} onClick={menuChange.bind(null, '月度')}>月度</div>
-            </div>
-          : null
-        }
+        <Chart assetsStore={assetsStore} />
       </div>
-      <Chart assetsStore={assetsStore} />
-    </div>
-  );
+    );
+  }
 }
 
 Analysis.propTypes = {
   assetsStore: PropTypes.object,
 };
+
 export default observer(Analysis);
