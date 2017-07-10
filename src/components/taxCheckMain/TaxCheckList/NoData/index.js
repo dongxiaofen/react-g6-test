a7f83429861d06c234da70de2de917df64718655
谢明 <ming.xie@socialcredits.cn>
2017-07-10 15:58:13 +0800

    修改没有内容显示的图片

--- src/components/taxCheckMain/TaxCheckList/NoData/index.js
+++ src/components/taxCheckMain/TaxCheckList/NoData/index.js
@@ -1,13 +1,13 @@
 import React, {PropTypes} from 'react';
 import { observer } from 'mobx-react';
 import styles from './index.less';
-import noneDataImage from 'imgs/personCheck/personNoData.png';
+import noneDataImage from 'imgs/tax/noData.png';
 
 function NoneData({}) {
   return (
     <div className={styles.noneData}>
       <img src={noneDataImage} alt="noneData" />
-      <p>尚未进行企业年度报税核查，请添加</p>
+      <p>尚未进行企业经营核查，请添加</p>
     </div>
   );
 }
