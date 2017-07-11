4e86c7fd1ad2a5c5b1fff81f1732abd248f8a6dc
谢明 <ming.xie@socialcredits.cn>
2017-07-11 15:11:18 +0800

    修改没有数据时显示核查样例

--- src/components/taxCheckMain/TaxCheckList/NoData/index.js
+++ src/components/taxCheckMain/TaxCheckList/NoData/index.js
@@ -1,23 +1,21 @@
 import React, {PropTypes} from 'react';
-import { observer, inject } from 'mobx-react';
+import {observer, inject} from 'mobx-react';
 import styles from './index.less';
-import noneDataImage from 'imgs/tax/noData.png';
+// import noneDataImage from 'imgs/tax/noData.png';
 import noneDataImageStop from 'imgs/tax/tax_check_example.png';
 
-function NoneData({taxCheckStore}) {
-  if (!taxCheckStore.taxCheckAvailable) {
-    return (
-      <div className={styles.none_dataImage_Stop}>
-        <img src={noneDataImageStop} alt="noneDataImage_Stop" />
-      </div>
-    );
-  }
+function NoneData({}) {
   return (
-    <div className={styles.noneData}>
-      <img src={noneDataImage} alt="noneData" />
-      <p>尚未进行企业经营核查，请添加</p>
+    <div className={styles.none_dataImage_Stop}>
+      <img src={noneDataImageStop} alt="noneDataImage_Stop"/>
     </div>
   );
+  // return (
+  //   <div className={styles.noneData}>
+  //     <img src={noneDataImage} alt="noneData" />
+  //     <p>尚未进行企业经营核查，请添加</p>
+  //   </div>
+  // );
 }
 
 NoneData.propTypes = {
