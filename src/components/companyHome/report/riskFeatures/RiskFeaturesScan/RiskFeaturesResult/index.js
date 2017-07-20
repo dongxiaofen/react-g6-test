c814b67cab1e9a0df4ec409c6e16e4d13a3a96f4
luoyang <yang.luo@socialcredits.cn>
2017-07-20 13:53:35 +0800

    修改风险扫描和六芒星字段

--- src/components/companyHome/report/riskFeatures/RiskFeaturesScan/RiskFeaturesResult/index.js
+++ src/components/companyHome/report/riskFeatures/RiskFeaturesScan/RiskFeaturesResult/index.js
@@ -2,13 +2,13 @@ import React, {PropTypes} from 'react';
 import { observer } from 'mobx-react';
 import styles from './index.less';
 import Loading from './Loading';
-// import Result from './Result';
+import Result from './Result';
 
 function RiskFeaturesResult({reportAxisStore}) {
   return (
     <div className={styles.box}>
-      {/* {reportAxisStore.riskLoading ? <Loading /> : <Result data={reportAxisStore} />} */}
-      {reportAxisStore.riskLoading ? <Loading /> : <Loading />}
+      {reportAxisStore.riskLoading ? <Loading /> : <Result data={reportAxisStore} />}
+      {/* {reportAxisStore.riskLoading ? <Loading /> : <Loading />} */}
     </div>
   );
 }
