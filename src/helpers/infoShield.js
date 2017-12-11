2669d912f22360f37c579762ea5e19bb202b3e56
xiaofen <xiaofen.dong@socialcredits.cn>
2017-12-11 11:50:31 +0800

    fix: 安全隐私由显示４位数变为显示２位数

--- src/helpers/infoShield.js
+++ src/helpers/infoShield.js
@@ -1,8 +1,8 @@
 import __repeat from 'lodash/repeat';
 export function shieldInfo(data) {
-  const start = data.substring(0, 4);
-  const end = data.substring(data.length - 5, data.length - 1);
-  const shieldData = start + __repeat('*', data.length - 8) + end;
+  const start = data.substring(0, 2);
+  const end = data.substring(data.length - 3, data.length - 1);
+  const shieldData = start + __repeat('*', data.length - 4) + end;
   // const shieldData = start + '**********' + end;
   return shieldData;
 }
