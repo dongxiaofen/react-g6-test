f1033d69e35d5a2b39b531e8627b70659efbdafb
xiaofen <xiaofen.dong@socialcredits.cn>
2018-02-03 11:18:54 +0800

    feat: 接入说明更换

--- src/components/v2/apiListDetail/detail/introduce/index.js
+++ src/components/v2/apiListDetail/detail/introduce/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import Highlight from 'react-fast-highlight';
-// import Table from 'components/common/Table';
+import Table from 'components/common/Table';
 import styles from './index.less';
-import { javaText } from './config';
+import { demo, example, headerDataSource, headerColumns } from './config';
 @observer
 export default class Introduce extends Component {
   componentDidMount() {
@@ -12,55 +12,81 @@ export default class Introduce extends Component {
   render() {
     return (
       <div className={styles.introduce}>
-        <div>
-          <div className={styles.box}>
-            <h1>1. API概述</h1>
-            <p className={styles.pcontent}>所有API都是使用https请求调用，请求域名: <a href="https://business.socialcredits.cn">https://business.socialcredits.cn</a></p>
-            <p className={styles.pcontent}>请求调用流程:</p>
-            <ol className={styles.list}>
-              <li>1). 客户端需先调用身份验证令牌(token)的"授权API"</li>
-              <li>2). "授权API"会返回一个身份验证令牌(token)</li>
-              <li>3). 客户端使用获取的身份令牌(token)来调用业务API</li>
-              <li>4). 业务API返回结果</li>
-            </ol>
-          </div>
-          <div className={styles.box}>
-            <h1>2. 认证方式</h1>
-            <p className={styles.pcontent}>所有业务API的调用都将使用token验证。一个有效的API调用需要包含一个有效身份验证令牌(token，通过JWT生成)。</p>
-          </div>
-          {/*<div style={{width: '50%', marginBottom: '10px'}}>
-            <Table dataSource={paramsDataSource} columns={paramsColumns}/>
-          </div>*/}
-          <p className={styles.pcontent}>sc-api-token生成规则:</p>
+        <div className={styles.box}>
+          <h1>1. API概述</h1>
+          <p>所有API都是使用https请求调用，请求域名: <a href="https://business.socialcredits.cn">https://business.socialcredits.cn</a></p>
+          <p>请求调用流程:</p>
+          <ol className={styles.list}>
+            <li>1). 客户端需先调用身份验证令牌(token)的"授权API"</li>
+            <li>2). "授权API"会返回一个身份验证令牌(token)</li>
+            <li>3). 客户端使用获取的身份令牌(token)来调用业务API</li>
+            <li>4). 业务API返回结果</li>
+          </ol>
+        </div>
+        <div className={styles.box}>
+          <h1>2. 认证方式</h1>
+          <p>所有业务API的调用都将使用token验证。一个有效的API调用需要包含一个有效身份验证令牌(token，通过JWT生成)。</p>
+          <h3>2.1. 客户端调用"获取授权API"获取Token的流程</h3>
+          <ol className={styles.list}>
+            <li>1). 客户端需要使用用户的[searedSecret]和[apiKey] 调用"获取授权API"获取token</li>
+            <li>2). API网关收到请求后,后端服务会认证您的apiKey,通过后会生成一个token,并返回给您,您可凭此Token来调用"业务API"</li>
+            <li>3). 客户端使用"获取授权API" 得到的Token来调用"业务API"</li>
+          </ol>
+          <p>注意: [searedSecret]和[apiKey]在接口平台 个人中心->安全隐私即可查看, token有效时间为5分钟</p>
+          <h3>2.2. 身份验证令牌(token)获取</h3>
+          <p>请求方式: POST</p>
+          <p>Content-type: application/json;charset=UTF-8</p>
+          <p>{`请求url: http://<接口地址>/api/v2/sc/token`}</p>
+          <p>例如：　https://business.socialcredits.cn/api/v2/sc/token</p>
+          <h3>2.3. 请求body</h3>
           <Highlight
             languages={['javascript']}
             className="my-class"
             >
             {`
-              var s = [http method] + [sharedSecret] + [timestamp] + [API uri path] + [queryParam]
-              var token = sha256Hex(s)
+              {
+                "searedSecret":"你的密钥",
+                "apiKey":"你的apiKey"
+              }
             `}
           </Highlight>
-          <ol className={styles.list}>
-            <li>参数追加过程不需要空格进行分割</li>
-            <li>接口传输的所有参数使用 UTF8 编码</li>
-            <li>API uri path 不包含协议和域名部分，也不包含查询参数。如：/api/external/enterprise/report</li>
-            <li>使用SHA-256 hash，Hex encode</li>
-            <li>http method 使用小写</li>
-            <li>需要传递请求参数时，需要把参数部分也加入token生成。参数为原始查询数据字符串（中文需要urlencode）。 如：/api/external/user/customerLabels?externalId=xxxx&labels=eduction，则queryParam为：externalId=xxxx&labels=eduction</li>
-            <li>POST body 部分不加校验范围，GET方式传输中文时，需要先进行UrlEncode。</li>
-          </ol>
-          <p className={styles.pcontent}>代码示例（java）</p>
+          <h3>2.4. 返回结果</h3>
+          <p>在成功调用后，API服务将返回以下JSON格式的sc-api-token：</p>
+          <Highlight
+            languages={['javascript']}
+            className="my-class"
+            >
+            {`
+              {
+                "data": {
+                  "sc-api-token":"******"
+                },
+                "errorCode":200,
+                "message":"ok"
+              }
+            `}
+          </Highlight>
+          <h3>2.5. 使用token</h3>
+          <p>所有业务api请求都必须带上sc-api-token,它必须添加在HTTP header中，然后请求业务API.</p>
+          <p>{`"sc-api-token":<sc-api-token值>`}</p>
+          <h3>２.6．获取示例</h3>
           <Highlight
             languages={['java']}
             className="my-class"
             >
-            {javaText}
+            {demo}
           </Highlight>
         </div>
-        <div>
-          <h1>返回说明：</h1>
-          <p className={styles.pcontent}>正确返回时,errorCode 为  200 , message为  success , data 字段里的内容是api返回的结果</p>
+        <div className={styles.box}>
+          <h1>3.业务api调用示例</h1>
+          <Highlight
+            languages={['java']}
+            className="my-class"
+            >
+            {example}
+          </Highlight>
+          <h3>3.1　返回说明</h3>
+          <p>正确返回时,errorCode 为 200 , message为 success , data 字段里的内容是api返回的结果</p>
           <Highlight
             languages={['javascript']}
             className="my-class"
@@ -75,8 +101,7 @@ export default class Introduce extends Component {
               }
             `}
           </Highlight>
-          <p>错误返回时,errorCode 为错误码，message 为错误信息, 可以在全局错误码里查看, data为 null </p>
-          <p>example:</p>
+          <p>错误返回时,errorCode 为错误码，message 为错误信息, 可以在全局错误码里查看, data为 null</p>
           <Highlight
             languages={['javascript']}
             className="my-class"
@@ -90,8 +115,8 @@ export default class Introduce extends Component {
             `}
           </Highlight>
         </div>
-        <div>
-          <h1>API 调用频率限制</h1>
+        <div className={styles.box}>
+          <h1>4.　API 调用频率限制</h1>
           <p>每小时调用API的频率限制，默认是500, 如果不够请联系管理员</p>
           <p>可以通过API请求查看header:</p>
           <Highlight
@@ -108,10 +133,10 @@ export default class Introduce extends Component {
               X-RateLimit-Reset: 1372700873
             `}
           </Highlight>
-          <p>Header 信息说明</p>
-          {/*<div style={{width: '50%', marginBottom: '10px'}}>
+          <h3>4.1　Header 信息说明</h3>
+          <div style={{width: '50%', marginBottom: '10px'}}>
             <Table dataSource={headerDataSource} columns={headerColumns}/>
-          </div>*/}
+          </div>
         </div>
       </div>
     );
