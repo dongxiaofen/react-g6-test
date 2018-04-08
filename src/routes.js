e51043e8c0cc353949bf7fbae83fa91536d62230
xiaofen <xiaofen.dong@socialcredits.cn>
2018-04-08 11:15:52 +0800

    feat: 初始化项目

--- src/routes.js
+++ src/routes.js
@@ -5,48 +5,13 @@ import {Route, IndexRoute} from 'react-router';
 import {
     App,
     Login,
-    // Interface,
-    Detail,
-    Introduce,
-    Test,
-    // Consume,
-    Consumption,
-    Recharge,
-    // Account,
-    Modify,
-    Myapi,
-    Safe,
-    IntroduceV2,
-    ApiListDetail,
-    ApiTest,
-    ConsumptionV2,
-    RechargeV2,
-    ModifyV2,
-    MyapiV2,
-    SafeV2,
   } from 'containers';
 
 export default () => {
   return (
     <Route path="/" component={ App }>
-      <IndexRoute component={ IntroduceV2 } />
+      <IndexRoute component={ Login } />
       <Route path="login" component={ Login } />
-      <Route path="v1/introduce" component={ Introduce } />
-      <Route path="v1/detail" component={ Detail } />
-      <Route path="v1/test" component={ Test } />
-      <Route path="v1/consumption" component={ Consumption } />
-      <Route path="v1/recharge" component={ Recharge } />
-      <Route path="v1/modify" component={ Modify } />
-      <Route path="v1/myapi" component={ Myapi } />
-      <Route path="v1/safe" component={ Safe } />
-      <Route path="v2/introduce" component={ IntroduceV2 } />
-      <Route path="v2/detail" component={ ApiListDetail } />
-      <Route path="v2/test" component={ ApiTest } />
-      <Route path="v2/consumption" component={ ConsumptionV2 } />
-      <Route path="v2/recharge" component={ RechargeV2 } />
-      <Route path="v2/modify" component={ ModifyV2 } />
-      <Route path="v2/myapi" component={ MyapiV2 } />
-      <Route path="v2/safe" component={ SafeV2 } />
     </Route>
   );
 };
